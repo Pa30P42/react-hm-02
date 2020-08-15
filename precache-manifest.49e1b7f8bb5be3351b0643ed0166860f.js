ed9d491b2b309f98a848154ce9eca97243012d47
Ismayil <hajiev93@gmail.com>
2020-08-15 10:17:24 +0300

    Updates

---
+++ precache-manifest.49e1b7f8bb5be3351b0643ed0166860f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc53f598f4faef4ed3df89ebeb246038",
+    "url": "/react-hm-02/index.html"
+  },
+  {
+    "revision": "82004d4409b3c32118d3",
+    "url": "/react-hm-02/static/js/2.64696d80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hm-02/static/js/2.64696d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c28ef97e6e11d492df",
+    "url": "/react-hm-02/static/js/main.0341bc8a.chunk.js"
+  },
+  {
+    "revision": "e45b39757258654a4d69",
+    "url": "/react-hm-02/static/js/runtime-main.59af1c69.js"
+  }
+]);
